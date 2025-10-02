9385c32dcebc600be0ebf00353934f781e1c37b9
diegopena557 <dipenaro@gmail.com>
2025-10-02 13:48:19 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -69,7 +69,6 @@ canvas_result = st_canvas(
     width=canvas_width,
     drawing_mode=drawing_mode,
     key=f"canvas_{canvas_width}_{canvas_height}",
-    drawing_mode=drawing_mode,
     key="canvas",
 )
 
